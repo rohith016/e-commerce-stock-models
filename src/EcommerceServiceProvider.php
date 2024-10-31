4d38a9f85ac8ed35981f3fd4e7c445442cbfc8a7
rohith <rohithdpr016@gmail.com>
2024-10-31 22:07:06 +0530

    update service provider

--- src/EcommerceServiceProvider.php
+++ src/EcommerceServiceProvider.php
@@ -9,23 +9,33 @@ class EcommerceServiceProvider extends ServiceProvider
     public function boot()
     {
         // Publish migrations
+        // php artisan vendor:publish --tag=ecommerce-migrations
         $this->publishes([
             __DIR__ . '/Migrations' => database_path('migrations'),
         ], 'ecommerce-migrations');
 
         // Publish models
+        // php artisan vendor:publish --tag=ecommerce-models
         $this->publishes([
             __DIR__ . '/Models' => app_path('Models'),
         ], 'ecommerce-models');
 
+        
         // Publish seeders
+        // php artisan vendor:publish --tag=ecommerce-seeders
         $this->publishes([
             __DIR__ . '/Seeders' => database_path('seeders'),
         ], 'ecommerce-seeders');
 
         // Publish factories
+        // php artisan vendor:publish --tag=ecommerce-factories
         $this->publishes([
             __DIR__ . '/Factories' => database_path('factories'),
         ], 'ecommerce-factories');
+
+
+        // publish all things like run this command 
+        // php artisan vendor:publish
+
     }
 }
