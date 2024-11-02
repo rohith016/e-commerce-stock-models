6cfa8bca58b44f9649afa4f860277bd93d726fda
rohith <rohithdpr016@gmail.com>
2024-11-02 13:37:04 +0530

    add order model relations

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -4,7 +4,10 @@ namespace App\Models;
  
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
- 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\hasOne;
+
 class Order extends Model
 {
     use SoftDeletes;
@@ -20,9 +23,80 @@ class Order extends Model
         'total_amount',
         'status'
     ];
-
-    // relationship between order and user
-    public function user(){
+    /**
+     * Relationship between Order and User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
+    /**
+     * Relationship between Order and OrderItem
+     * 
+     * An Order can have multiple OrderItems
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    /**
+     * Relationship between Order and CouponUsage
+     * 
+     * An Order can have CouponUsage
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function couponUsage(): HasOne
+    {
+        return $this->hasOne(CouponUsage::class);
+    }
+    /**
+     * Relationship between Order and OrderReturn
+     * 
+     * An Order can have OrderReturn
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function orderReturn(): HasOne
+    {
+        return $this->hasOne(OrderReturn::class);
+    }
+    /**
+     * Relationship between Order and Payment
+     * 
+     * An Order can have Payment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
+    /**
+     * Relationship between Order and Refund
+     * 
+     * An Order can have Refund
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function refund(): HasOne
+    {
+        return $this->hasOne(Refund::class);
+    }
+    /**
+     * Relationship between Order and Shipment
+     * 
+     * An Order can have Shipment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function shipment(): HasOne
+    {
+        return $this->hasOne(Shipment::class);
+    }
+    
 }
